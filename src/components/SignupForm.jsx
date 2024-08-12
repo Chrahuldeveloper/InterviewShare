63e6c7bfc0dfb46758c8a0a5e1820ae63754e4f2
Rahul <chrahulofficial@gmail.com>
2024-08-12 18:45:17 +0530

    spd

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { FaGoogle } from "react-icons/fa";
 
 export default function SignupForm() {
   return (
-    <div className="max-w-sm md:max-w-md mx-auto mt-8">
+    <div className=" max-w-md mx-auto mt-8 border-[1px] p-8 border-gray-300 rounded-lg">
       <div className="text-slate-800 space-y-4">
         <h1 className="text-2xl font-bold">SignUp</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -30,8 +31,21 @@ export default function SignupForm() {
           </button>
         </div>
       </form>
-      <div>
-        
+
+      <div className="my-6">
+        <h1 className="text-center text-gray-400 font-semibold">
+          --Or Sign Up using--
+        </h1>
+        <div className="flex justify-center mt-4 border-[1px] w-14 h-14 items-center rounded-full mx-auto p-4">
+          <FaGoogle size={40} />
+        </div>
+      </div>
+
+      <div className="text-center">
+        <p>
+          Already a User?
+          <span className="text-blue-500 font-bold"> Login</span>
+        </p>
       </div>
     </div>
   );
