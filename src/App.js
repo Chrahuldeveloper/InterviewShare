4ee2146ec9aae53c36871424a13d1398d39afc36
Rahul <chrahulofficial@gmail.com>
2024-08-25 14:59:54 +0530

    done

--- src/App.js
+++ src/App.js
@@ -9,19 +9,31 @@ import {
   UploadExperience,
   WriteBlog,
 } from "./Pages";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
+
 export default function App() {
+  const jwt = localStorage.getItem("jwt");
+
   return (
     <Routes>
-      <Route path="/" element={<SignUp />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/interview/:id" element={<ReadFullExperience />} />
-      <Route path="/user/:userid" element={<Profile />} />
-      <Route path="/blog/:blogid" element={<ReadFullBlog />} />
-      <Route path="/upload/experience" element={<UploadExperience />} />
-      <Route path="/blogs" element={<Blogs />} />
-      <Route path="/write/blog" element={<WriteBlog />} />
+      {jwt ? (
+        <>
+          <Route path="/home" element={<Home />} />
+          <Route path="/interview/:id" element={<ReadFullExperience />} />
+          <Route path="/user/:userid" element={<Profile />} />
+          <Route path="/blog/:blogid" element={<ReadFullBlog />} />
+          <Route path="/upload/experience" element={<UploadExperience />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/write/blog" element={<WriteBlog />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </>
+      ) : (
+        <>
+          <Route path="/" element={<SignUp />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </>
+      )}
     </Routes>
   );
 }
